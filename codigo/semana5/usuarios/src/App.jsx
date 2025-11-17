6c230618eddf4e5e940a6a35583792188bf8189e
Eric Aguayo <ericmaster@nimblersoft.com>
2025-11-17 21:59:20 +0000

    useEffect

--- codigo/semana5/usuarios/src/App.jsx
+++ codigo/semana5/usuarios/src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //   const usuarios = [{
+  //     id: 1,
+  //     name: 'Usuario1',
+  //     email: 'usuario1@test.com'
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Usuario2',
+  //     email: 'usuario2@test.com'
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Usuario3',
+  //     email: 'usuario3@test.com'
+  //   },
+  // ]
+  const [usuarios, setUsuarios] = useState([])
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  useEffect(async () => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    try {
+      const response = await fetch(url);
+      if (response.ok === true) {
+        // console.log(response.json())
+        setUsuarios(await response.json())
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Lista de Usuarios</h1>
+      <ul>
+        {usuarios.map((usuario) => {
+          return <li key={usuario.id}>
+            <h2><strong>Nombre: </strong> {usuario.name}</h2>
+            <p><strong>correo:</strong> {usuario.email}</p>
+          </li>
+        })}
+      </ul>
     </>
   )
 }
